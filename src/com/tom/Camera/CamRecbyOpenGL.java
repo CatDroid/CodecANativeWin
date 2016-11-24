c5dfbf432fb4e0c140e125e51ccb782ebc52ed80
hanloong ho <1198432354@qq.com>
2016-11-24 15:06:28 +0800

    添加OpenGLsharder的注释(Camera使用OpenGL显示)

--- src/com/tom/Camera/CamRecbyOpenGL.java
+++ src/com/tom/Camera/CamRecbyOpenGL.java
@@ -39,6 +39,7 @@ import android.opengl.EGLSurface;
 import android.opengl.GLES11Ext;
 import android.opengl.GLES20;
 import android.opengl.Matrix;
+import android.graphics.BitmapFactory;
 import android.graphics.SurfaceTexture;
 
 public class CamRecbyOpenGL extends Activity {
@@ -62,9 +63,14 @@ public class CamRecbyOpenGL extends Activity {
  
 	
 	// Fragment shader that swaps color channels around.
-	private static final String SWAPPED_FRAGMENT_SHADER = "#extension GL_OES_EGL_image_external : require\n"
-			+ "precision mediump float;\n" + "varying vec2 vTextureCoord;\n" + "uniform samplerExternalOES sTexture;\n"
-			+ "void main() {\n" + "  gl_FragColor = texture2D(sTexture, vTextureCoord).gbra;\n" + "}\n";
+	private static final String SWAPPED_FRAGMENT_SHADER = 
+			"#extension GL_OES_EGL_image_external : require\n"
+			+ "precision mediump float;\n" 
+			+ "varying vec2 vTextureCoord;\n" 
+			+ "uniform samplerExternalOES sTexture;\n"
+			+ "void main() {\n" 
+			+ "  gl_FragColor = texture2D(sTexture, vTextureCoord).gbra;\n" +  // 注意 颜色的排列改变了 
+			"}\n";
 
 	// encoder / muxer state
 	private MediaCodec mEncoder;
@@ -956,21 +962,31 @@ public class CamRecbyOpenGL extends Activity {
 		private static final int TRIANGLE_VERTICES_DATA_UV_OFFSET = 3;
 		private final float[] mTriangleVerticesData = {
 				// X, Y, Z, U, V
-				-1.0f, -1.0f, 0, 0.f, 0.f, 1.0f, -1.0f, 0, 1.f, 0.f, -1.0f, 1.0f, 0, 0.f, 1.f, 1.0f, 1.0f, 0, 1.f,
-				1.f, };
+				-1.0f, -1.0f, 0, 0.f, 0.f,  /*顶点坐标     纹理坐标*/
+				 1.0f, -1.0f, 0, 1.f, 0.f, 
+				 -1.0f, 1.0f, 0, 0.f, 1.f,
+				  1.0f, 1.0f, 0, 1.f, 1.f, };
 
 		private FloatBuffer mTriangleVertices;
 
-		private static final String VERTEX_SHADER = "uniform mat4 uMVPMatrix;\n" + "uniform mat4 uSTMatrix;\n"
-				+ "attribute vec4 aPosition;\n" + "attribute vec4 aTextureCoord;\n" + "varying vec2 vTextureCoord;\n"
-				+ "void main() {\n" + "    gl_Position = uMVPMatrix * aPosition;\n"
-				+ "    vTextureCoord = (uSTMatrix * aTextureCoord).xy;\n" + "}\n";
+		private static final String VERTEX_SHADER = 
+				"uniform mat4 uMVPMatrix;\n" + 
+				"uniform mat4 uSTMatrix;\n"
+				+ "attribute vec4 aPosition;\n" 
+				+ "attribute vec4 aTextureCoord;\n" 
+				+ "varying vec2 vTextureCoord;\n"
+				+ "void main() {\n" 
+				+ "    gl_Position = uMVPMatrix * aPosition;\n"
+				+ "    vTextureCoord = (uSTMatrix * aTextureCoord).xy;\n" 
+				+ "}\n";
 
 		private static final String FRAGMENT_SHADER = "#extension GL_OES_EGL_image_external : require\n"
-				+ "precision mediump float;\n" + // highp here doesn't seem to
-													// matter
-				"varying vec2 vTextureCoord;\n" + "uniform samplerExternalOES sTexture;\n" + "void main() {\n"
-				+ "    gl_FragColor = texture2D(sTexture, vTextureCoord);\n" + "}\n";
+				+ "precision mediump float;\n"  // highp here doesn't seem to matter
+				+ "varying vec2 vTextureCoord;\n"
+				+ "uniform samplerExternalOES sTexture;\n" 
+				+ "void main() {\n"
+				+ "    gl_FragColor = texture2D(sTexture, vTextureCoord);\n" 
+				+ "}\n";
 
 		private float[] mMVPMatrix = new float[16];
 		private float[] mSTMatrix = new float[16];
@@ -1035,9 +1051,10 @@ public class CamRecbyOpenGL extends Activity {
 			GLES20.glUseProgram(mProgram);
 			checkGlError("glUseProgram");
 
-			GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
-			GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, mTextureID);
-
+			GLES20.glActiveTexture(GLES20.GL_TEXTURE0);	// 把  纹理单元 0 设置为当前纹理单元 
+			GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, mTextureID); // 纹理目标 绑定到 当前纹理单元
+										
+			// 顶点坐标 
 			mTriangleVertices.position(TRIANGLE_VERTICES_DATA_POS_OFFSET);
 			GLES20.glVertexAttribPointer(maPositionHandle, 3, GLES20.GL_FLOAT, false,
 					TRIANGLE_VERTICES_DATA_STRIDE_BYTES, mTriangleVertices);
@@ -1045,13 +1062,14 @@ public class CamRecbyOpenGL extends Activity {
 			GLES20.glEnableVertexAttribArray(maPositionHandle);
 			checkGlError("glEnableVertexAttribArray maPositionHandle");
 
+			// 纹理坐标   纹理坐标  和  顶点坐标都在 一个ByteBuffer中 
 			mTriangleVertices.position(TRIANGLE_VERTICES_DATA_UV_OFFSET);
 			GLES20.glVertexAttribPointer(maTextureHandle, 2, GLES20.GL_FLOAT, false,
 					TRIANGLE_VERTICES_DATA_STRIDE_BYTES, mTriangleVertices);
 			checkGlError("glVertexAttribPointer maTextureHandle");
 			GLES20.glEnableVertexAttribArray(maTextureHandle);
 			checkGlError("glEnableVertexAttribArray maTextureHandle");
-
+			
 			Matrix.setIdentityM(mMVPMatrix, 0);
 			GLES20.glUniformMatrix4fv(muMVPMatrixHandle, 1, false, mMVPMatrix, 0);
 			GLES20.glUniformMatrix4fv(muSTMatrixHandle, 1, false, mSTMatrix, 0);
